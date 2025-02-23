d8f843210c403f263d7e8badfe3ca12fac843c76
Vova <lololoskafry@gmail.com>
2025-02-23 19:49:20 +0200

    Add project files.

---
+++ 22.01.25/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace _22._01._25
+{
+    internal class ApplicationContext : DbContext
+    {
+            public DbSet<Book> Books => Set<Book>();
+
+            public ApplicationContext() => Database.EnsureCreated();
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-S17FGQD\SQLEXPRESS;Database=firstapp;TrustServerCertificate=True;Trusted_Connection=True;");
+            }
+        
+    }
+}
---
+++ 22.01.25/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _22._01._25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new())
+            {
+                // создаем два объекта User
+                Book book1 = new Book { Name = "Book 1", Author = "Author 1", YearPublished = 2022 };
+                Book book2 = new Book { Name = "Book 2", Author = "Author 2", YearPublished = 2021 };
+
+                // добавляем их в бд
+                db.Books.Add(book1);
+                db.Books.Add(book2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var books = db.Books.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (Book u in books)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name}, {u.Author} - {u.YearPublished}");
+                }
+            }
+        }
+    }
+}
